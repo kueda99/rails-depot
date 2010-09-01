f62fdf48283e64cfe5b881a9c75556de169e2873
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2010-09-01 19:34:52 +0900

    まっさらのアプリケーション

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0f11e7ea625fd34e4c6ef89becb160def28e1d3ed7f7cf1d276baaf24383fd8e0dd2d9c83d9f327ff58a930b57ba1b5c30dc7d5805605069211206364a4d65c9'
