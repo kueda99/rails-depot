14ad6d0c8176237f373af8a4670a0ecc00794209
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2010-09-04 10:39:18 +0900

    store#index を実装
    コミット 52130599 で作成した store コントローラおよびビューを実装した。
    また、store#index で返されるページのテストも実装した。

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
@@ -6,4 +7,15 @@ class StoreControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  # すべての製品 (タグが entry クラス) について、price_line クラスのタ
+  # グがあり、画像へのソースタグを持っていることをテストする。
+  test "should get index contains 3 entries with expected tags" do
+    get :index
+    assert_select 'div.entry' do |elements|
+      elements.each do |element|
+        assert_select element, 'div.price_line'
+        assert_select element, %{img[src=?]}, /.*\.(gif|jpg|png).*/i
+      end
+    end
+  end
 end
