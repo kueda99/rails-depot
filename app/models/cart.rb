9f7009d9e81ac49e66d077cc7589d47efe7585d7
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2010-09-06 22:36:16 +0900

    Cart#add_product(product_id) を実装
    製品をカートに入れるメソッドである。これまではコントローラの方で
    LineItem インスタンスを生成し、カートに追加していたが、Cart モデルにそ
    の役割を任すのが自然である。
    
    もし Cart の中に Product への関連を持つ LineItem がすでにあった場合、新
    たに LineItem を生成せずに、既存のインスタンスの quantity 属性値を 1 つ
    増加させる。

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
+
+  # Cart#add_product(product_id) は、関連するラインアイテム
+  # (line_items) を 1 つ増やすか、またはすでにその製品がカートにあった
+  # 場合は、その製品のためのラインアイテムの属性 quantity を 1 つ増やす。
+  def add_product(product_id)
+    current_item = line_items.detect { |item| item.product_id == product_id }
+    if current_item.nil?
+      current_item = self.line_items.build(:product_id => product_id)
+    else
+      current_item.quantity += 1
+    end
+    current_item
+  end
+
 end
--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -13,4 +13,35 @@ class CartTest < ActiveSupport::TestCase
     cart.destroy
     assert_equal 0, cart.line_items.count
   end
+
+  # Cart#add_product(product_id) のテスト その1: 引数 product_id の製
+  # 品がカートにない場合、新規に LineItem インスタンスを作成して返す。
+  test "should create a line item if a given product is NOT in the cart" do
+    cart = carts(:cart_with_two_items)
+    ruby_id = products(:ruby).id
+    assert_nil cart.line_items.detect { |item| item.product_id == ruby_id }
+    new_line_item = cart.add_product(products(:ruby).id)
+    assert_equal 1, new_line_item.quantity
+    assert_not_nil cart.line_items.detect { |item| item.product_id == ruby_id }
+  end
+
+  # Cart#add_product(product_id) のテスト その2: 引数 product_id の製品
+  # がすでにカートにある場合、既存の LineItem インスタンスの quantity
+  # が 1 つ増える。
+  test "should increment quantity of a line item if a given product is in the cart" do
+    cart = carts(:cart_with_two_items)
+    product_one_id = products(:one).id
+
+    item_in_the_cart = cart.line_items.detect { |item| item.product_id == product_one_id }
+    assert_not_nil item_in_the_cart
+
+    # add_product() によって LineItem インスタンスの quantity の値が 1
+    # つ増えることの確認
+    assert_difference 'item_in_the_cart.quantity' do
+      new_line_item = cart.add_product(product_one_id)
+      assert_equal item_in_the_cart, new_line_item
+    end
+
+    assert_not_nil cart.line_items.detect { |item| item.product_id == product_one_id }
+  end
 end
