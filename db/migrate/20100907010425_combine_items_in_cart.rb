370fa43bc3fbb5fb2c9296f0e0cdf0993c463f46
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2010-09-07 11:23:20 +0900

    ラインアイテムを 1 つのアイテムごとに分割するマイグレーションの作成
    ラインアイテムを製品毎に整理するマイグレーションをコミットした
    (61e387b) が、その逆の操作を db:rollback で実行できるようにした。
    
    db:migrate を実行したときに個々のラインアイテムの数量の情報は失なわれて
    いるので、ロールバックしてもデータベースが完全に元に戻ることはない。

--- db/migrate/20100907010425_combine_items_in_cart.rb
+++ db/migrate/20100907010425_combine_items_in_cart.rb
@@ -23,5 +23,18 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def self.down
+    # split items with quantity>1 into multiple items
+    LineItem.where("quantity > 1").each do |lineitem|
+      # add individual items
+      lineitem.quantity.times do
+        LineItem.create do |i|
+          i.cart_id = lineitem.cart_id
+          i.product_id = lineitem.product_id
+          i.quantity = 1
+        end
+        # remove original item
+      end
+      lineitem.destroy
+    end
   end
 end
