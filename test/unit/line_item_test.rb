3b474f46a91f9a4d4bd4f3ed8c1b09d8909f78f5
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2010-09-07 12:13:54 +0900

    ラインアイテムの数量の指定なしではデフォルト値になることを確認するテストケースを追加
    ラインアイテムのデータベースのマイグレーションでデフォルト値を 1 とし
    たが (コミット 91b2ca6)、本当にそうなるかを確認するテストケースを追加
    した。

--- test/unit/line_item_test.rb
+++ test/unit/line_item_test.rb
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
   test "should have methods returning relations" do
     assert LineItem.method_defined?('cart')
     assert LineItem.method_defined?('product')
+  end
+
+  # 明示的に製品の数量が指定されなかったときは 1 となることの確認 (マイ
+  # グレーションでの設定による機能)
+  test "should have decimal quantity" do
+    line_item = LineItem.create do |item|
+      item.cart = Cart.first
+      item.product = Product.first
+    end
+    assert_equal 1, line_item.quantity
   end
 end
