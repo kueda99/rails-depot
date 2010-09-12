616ad81476b5a622cc48db8322a53f56fac2a907
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2010-09-12 18:52:21 +0900

    アクセスしている人が管理者であるかを判断するメソッドを実装 (ビュー用)
    管理者のときは true を返すメソッド admin? を実装した。ビューの中で使う。
    
    また管理者モードのときと顧客モードのときで表示の仕方を変えるために、
    HTML タグの class 属性において "admin" を指定しているが、いちいち
    session[:user_id] の値を参照しているのはかっこ悪い。より簡潔に記述する
    ために "admin_or_customer" メソッドを追加した。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
+
+  # 管理者である場合は true を返す
+  def admin?
+    !session[:user_id].nil?
+  end
+
+  # 管理者である場合は "admin" を、そうでない場合は "customer" の文字列
+  # を返す
+  def admin_or_customer
+    admin? ? "customer" : "admin"
+  end
+
 end
